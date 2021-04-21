93c335b578cda563c2db78e92facb50d031a70d4
igor <isakov.dev921@gmail.com>
2021-04-21 20:20:54 +0300

    19 sorting

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -54,6 +54,7 @@ namespace Repository
                 .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
                 .Search(employeeParameters.SearchTerm)
                 .OrderBy(e => e.Name)
+                .Sort(employeeParameters.OrderBy)
                 .ToListAsync();
 
             return PagedList<Employee>
